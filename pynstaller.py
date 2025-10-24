45e0d1bfd5ef667d807b76b58fe525bfd51f31f5
cells-OSS <open-source-version@proton.me>
2025-10-24 22:27:25 +0300

    BugFix

--- pynstaller.py
+++ pynstaller.py
@@ -96,7 +96,9 @@ config_dir = os.path.join(os.getenv("APPDATA"), "pynstaller")
 
 os.makedirs(config_dir, exist_ok=True)
 
-with open(config_dir, "profile_programming.conf") as f:
+programming_profile_path = os.path.join(config_dir)
+
+with open(programming_profile_path, "profile_programming.conf") as f:
     f.write(f"librewolf \n vscode \n python3 \n vscode-python \n")
 
 welcomeMessage_config_path = os.path.join(config_dir, "welcome_message.conf")
